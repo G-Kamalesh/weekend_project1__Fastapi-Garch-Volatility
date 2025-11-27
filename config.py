4dbf0ab6845d9d018bfb76154593d1dadc449c66
Kamalesh07 <162958434+G-Kamalesh@users.noreply.github.com>
2025-11-27 11:53:12 +0530

    code files
    I have used a 3-tier Architecture :-
    
    1. data.py - data layer (has sqlite database and api),
    2. main.py - interface(Fastapi endpoint)
    3. model.py - business layer (has business logic)

---
+++ config.py
@@ -0,0 +1,26 @@
+"""This module extracts information from your `.env` file so that
+you can use your AlphaVantage API key in other parts of the application.
+"""
+
+import os
+from pydantic_settings import BaseSettings
+
+
+def return_full_path(filename: str = ".env") -> str:
+    """Return the absolute path for `filename` located next to this file."""
+    absolute_path = os.path.abspath(__file__)
+    directory_name = os.path.dirname(absolute_path)
+    full_path = os.path.join(directory_name, filename)
+    return full_path
+
+
+class Settings(BaseSettings):
+    alpha_api_key: str
+    db_name: str
+    model_directory: str
+
+    class Config:
+        env_file = return_full_path(".env")
+
+
+settings = Settings()
